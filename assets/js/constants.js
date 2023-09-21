94211c3221a20ba5c71b894ea4282680925b6bac
ggramajo <gaston.gramajo@mercadolibre.com>
2023-09-21 19:14:24 -0300

    change ids

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,21 +1,26 @@
-const key = "002ca967329c859e62cdb71049066ee2";
-const token = "ZmUxNjNhMjQ4ZTBhZjA3ZWM0MDcxNWUwMWY0YzNkOWMwNWE5NmRkNjhiYzUwOWQwMzNmNmFmYjUzM2ZiMGE1NQ==";
+// const key = "002ca967329c859e62cdb71049066ee2";
+// const token = "ZmUxNjNhMjQ4ZTBhZjA3ZWM0MDcxNWUwMWY0YzNkOWMwNWE5NmRkNjhiYzUwOWQwMzNmNmFmYjUzM2ZiMGE1NQ==";
+// const key = "14ce07b3203f16ca21f9de6965e3b577";
+// const token = "YzQ4OTczZTZkMGM1ZWU1ZTI2ODgxNzBiNTkyOWEzNzIxY2VmMTE5YmJhZDk3MzVmODUwMDcwYjczMDZhZTQ3Ng==";
+const key = "14ce07b3203f16ca21f9de6965e3b577";
+const token = "QVRUQTQxM2MwM2Y2OTQ4NjQwYzNiZjMzNzVmMWJhODc2NTk5MWM5ZWNjMjI5NjZmZDEwZjRiNjVkOWNmMjVhOWMxNjcwNzkzREQ5OA==";
+// ATTA413c03f6948640c3bf3375f1ba8765991c9ecc22966fd10f4b65d9cf25a9c1670793DD98
 
 const fix = str => str.indexOf('?') !== -1 ? '&' : '?';
 
 const getUrl = url => `https://api.trello.com/1/${url + fix(url)}key=${key}&token=${atob(token)}`;
 
 //const trelloId = "5ba132d5e4b91d12ada08efc";
-const trelloListId = '5e822b3b92ce775397b9cdf8';
-const activeUsersListId = '5e8235cda2e4ec40d931979c';
+const trelloListId = '650ca678ab1007084203dc12';
+const activeUsersListId = '650ca67802c643dd67a3b650';
 
-const projectId = '5e8408130171f230fcddcb10';
-const adminUsersId = '5e822b41141a2507d5c962ca';
-const scoreId = '5e822b4bd8dc2c669b88c9b0';
+const projectId = '650ca6f09d43332d3d1a2ab6';
+const adminUsersId = '650caf14137eea77ff3a8b99';
+const scoreId = '650caf16136a3b5a425f0910';
 
-const boardId = '5dd54a2725bf3b8e854e64bb';
+const boardId = '650ca6788642e0a2f7d51d52';
 
-const licenciasCardId = '5e1f18c648719471ad0b546b';
+const licenciasCardId = '650cae6d3b10dfff5a78f249';
 const ignoreStaticCardsList = [licenciasCardId];
 
 const enableTimer = true;
